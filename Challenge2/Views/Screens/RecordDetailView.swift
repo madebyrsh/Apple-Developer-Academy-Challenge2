e89d987725cce671ed4b5dac6124da7320d347d9
madebyrsh <madebyrsh.dev@gmail.com>
2026-04-23 12:23:12 +0900

    Create New RecordDetailView.swift file

---
+++ Challenge2/Views/Screens/RecordDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  RecordDetailView.swift
+//  Challenge2
+//
+//  Created by Shayne Ryu on 4/23/26.
+//
+
+import SwiftUI
+
+struct RecordDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecordDetailView()
+}
